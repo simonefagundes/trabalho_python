d995d8ad05984f40297a10d0e82fc8ee42899a58
Simone <simonefagundessilva2606@gmail.com>
2024-06-19 14:00:26 -0300

    Adicionar todos os arquivos e pastas do projeto

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+valor_um= int(input("Insira o primeiro número: "))
+valor_dois= int(input("Insira o segundo número: "))
+soma = valor_um + valor_dois
+subtracao = valor_um - valor_dois
+multiplicacao = valor_um * valor_dois
+divisao = valor_um / valor_dois
+
+print("Resultado da soma: " +str(soma))
+print("Resultado da subtração: " +str(subtracao))
+print("Resultado da multiplicação: " +str(multiplicacao))
+print("Resultado da divisão: " +str(divisao))
---
+++ variaveis.py
@@ -0,0 +1,19 @@
+a = 15
+b = 20
+soma = a+b
+print("Resultado da soma: " +str(soma))
+
+c=25
+d=12
+subtracao = c-d
+print("Resultado da subtração: "+str(subtracao))
+
+e=64
+d=52
+multiplicacao = e*d
+print("Resultado da multiplicação: "+str(multiplicacao))
+
+f=65
+g=62
+divisao = f/g
+print("Resultado da divisão: "+str(divisao))
